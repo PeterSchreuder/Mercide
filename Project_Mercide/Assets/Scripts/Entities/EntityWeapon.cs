d6a53beb5ed84d4a0eab49f66f0c2712408ebe03
PeterSchreuderMA <24441@ma-web.nl>
2020-06-04 17:59:47 +0200

    Added - Weapon object logic

--- Project_Mercide/Assets/Scripts/Entities/EntityWeapon.cs
+++ Project_Mercide/Assets/Scripts/Entities/EntityWeapon.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class EntityWeapon : GlobalObject
 {
+    public WeaponTemplate weaponTemplate;
+
     protected GameObject bulletPrefab;
     protected AudioSource audioSource;
-
-    [SerializeField]
-    protected float fireRate = 1;
-
-
 
     protected bool canShoot = true;
 
@@ -19,30 +16,41 @@ public class EntityWeapon : GlobalObject
     {
         base.Start();
 
+        if (!weaponTemplate)
+            Debug.LogError("No weaponTemplate found!");
+
         audioSource = GetComponent<AudioSource>();
 
-        bulletPrefab = Resources.Load("Bullet") as GameObject;
+        bulletPrefab = Resources.Load(weaponTemplate.wpBulletResourceName) as GameObject;
     }
 
-
+    /// <summary>
+    /// Shoots the weapon if canShoot == true
+    /// </summary>
     public virtual void Shoot()
     {
         if (!canShoot)
             return;
 
+        // Disable shooting
         canShoot = false;
-        StartCoroutine(ShootTimer(fireRate));
+        StartCoroutine(ShootTimer(weaponTemplate.wpFireRate));
 
+        // Play weapon sound
         audioSource.pitch = Random.Range(0.95f, 1.10f);
-        audioSource.PlayOneShot(audioSource.clip);
+        audioSource.PlayOneShot(weaponTemplate.wpSound);
 
         GameObject _bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
         _bullet.GetComponent<Bullet>().TeamNumber = TeamNumber;
     }
 
+    /// <summary>
+    /// Enables shooting after X time
+    /// </summary>
+    /// <param name="_fireRate"></param>
+    /// <returns></returns>
     public IEnumerator ShootTimer(float _fireRate)
     {
-
         yield return new WaitForSeconds(_fireRate);
 
         canShoot = true;
---
+++ Project_Mercide/Assets/Scripts/Objects/Weapons/WeaponTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapons/TestWeapon")]
+public class WeaponTemplate : ScriptableObject
+{
+    public string wpName = "WeaponTemplate";
+
+    public float wpFireRate = 1;
+    public AudioClip wpSound;
+
+    public float wpDamage = 25;
+
+    public string wpBulletResourceName = "Weapons/Guns/Bullet";
+}
