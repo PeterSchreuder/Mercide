8f7d239170dd2df7281ab7d167ca3dd05d97e284
PeterSchreuderMA <24441@ma-web.nl>
2020-06-06 19:10:04 +0200

    Added - Camera dolly with follow script | Started - Building a test level

---
+++ Project_Mercide/Assets/Scripts/Utils/Camera/FollowPosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+    [SerializeField]
+    private bool startOnTarget = true;
+
+    [SerializeField]
+    private Transform targetPosition;
+
+    [SerializeField]
+    private float followSpeed = 0.05f;
+
+    private void Start()
+    {
+        if (startOnTarget)
+            FlyToPosition(transform.position, targetPosition.position, 60f);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 _position1 = transform.position;
+        Vector3 _position2 = targetPosition.position;
+
+        FlyToPosition(_position1, _position2, followSpeed);
+    }
+
+    /// <summary>
+    /// Flies this object from position A to B
+    /// </summary>
+    /// <param name="_position1">Position A</param>
+    /// <param name="_position2">Position B</param>
+    /// <param name="_followSpeed">Speed</param>
+    void FlyToPosition(Vector3 _position1, Vector3 _position2, float _followSpeed)
+    {
+        transform.position = Vector3.Lerp(_position1, _position2, _followSpeed);
+    }
+}
